012a484cc50b532a785ba7702e64d837f36bb5d0
EC2 Default User <ec2-user@ip-10-3-0-91.ap-southeast-1.compute.internal>
2019-10-14 05:05:03 +0000

    gets and posts working

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -4,39 +4,25 @@ import json
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    method = event.get('httpMethod','GET') 
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    if method == 'GET':
+        # Return the contents of index.html on GETs
+        with open('index.html', 'r') as f:
+            indexPage = f.read()
+        return {
+            "statusCode": 200,
+            "headers": {
+            'Content-Type': 'text/html',
+            },
+            "body": indexPage
+        }
+        
+    if method == 'POST':
+        # Todo: Configure post method to log posts
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Will log all posts"
+            }),
+        }
